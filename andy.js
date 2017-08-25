f801a8c7efc117fd929f44c690eac38aeddc18c2
wcw <wcw@qq.com>
2017-08-25 11:31:02 +0800

    123

---
+++ andy.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wcw on 2017/8/25.
+ */
+
+{
+    person:{
+        name:"andy",
+        website:"www.baidu.com",
+        email:"andy@126.com"
+    }
+}
---
+++ lily.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wcw on 2017/8/25.
+ */
+{
+    person:{
+        name:"lily",
+        website:"www.baidu.com",
+        email:"andy@126.com"
+    }
+}
